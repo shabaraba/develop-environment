56da227af39aa30336dd2a44f773f3ddfa3bff26
shabaraba <work.shabaraba@gmail.com>
2022-01-13 10:02:02 +0900

    - add php-laravel environment

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+{ # this ensures the entire script is downloaded #
+    test() {
+        curl -O https://raw.githubusercontent.com/shabaraba/dotfiles/main/.vimrc
+    }
+
+    if [ $# -ne 1 ]; then
+      echo "the number of this shell argument must be 1." 1>&2
+      exit 1
+    fi
+
+    if [ $1 -eq 'laravel' ]; then
+        test
+    fi
+} # this ensures the entire script is downloaded #
+
