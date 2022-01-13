6866eef061a200a601d43916cedd59d3a2b111fa
shabaraba <work.shabaraba@gmail.com>
2022-01-13 18:14:31 +0900

    - add react-native environment

--- init.sh
+++ init.sh
@@ -5,6 +5,11 @@
         cd laravel-environment && rm -rf .git
     }
 
+    setup_react_native() {
+        git clone https://github.com/shabaraba/react-native-environment.git
+        cd react-native-environment && rm -rf .git
+    }
+
     if [ $# -ne 1 ]; then
       echo "the number of this shell argument must be 1." 1>&2
       exit 1
@@ -12,6 +17,8 @@
 
     if [ $1 = 'laravel' ]; then
         setup_laravel
+    elif [ $1 = 'react-native' ]; then
+        setup_react_native
     fi
 } # this ensures the entire script is downloaded #
 
