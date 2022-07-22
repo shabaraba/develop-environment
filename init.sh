46fed2e3540820de78c3dd133366c5ed954463cf
shabaraba <work.shabaraba@gmail.com>
2022-07-22 14:03:06 +0900

    - gather each environment to this repository

--- init.sh
+++ init.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 { # this ensures the entire script is downloaded #
-    setup_laravel() {
-        git clone https://github.com/shabaraba/laravel-environment.git
-        cd laravel-environment && rm -rf .git
-    }
+    # setup_laravel() {
+    #     git clone https://github.com/shabaraba/setup-develop-environment
+    #     mv setup-develop-environment laravel-environment
+    #     mv laravel-environment/Frameworks/Laravel laravel-environment/
+    #     cd laravel-environment && rm -rf .git
+    # }
+    #
+    # setup_react_native() {
+    #     git clone https://github.com/shabaraba/react-native-environment.git
+    #     cd react-native-environment && rm -rf .git
+    # }
+    #
+    # if [ $# -ne 1 ]; then
+    #   echo "the number of this shell argument must be 1." 1>&2
+    #   exit 1
+    # fi
+    #
 
-    setup_react_native() {
-        git clone https://github.com/shabaraba/react-native-environment.git
-        cd react-native-environment && rm -rf .git
-    }
-
-    if [ $# -ne 1 ]; then
-      echo "the number of this shell argument must be 1." 1>&2
-      exit 1
-    fi
-
-    if [ $1 = 'laravel' ]; then
-        setup_laravel
-    elif [ $1 = 'react-native' ]; then
-        setup_react_native
-    fi
+    git clone https://github.com/shabaraba/setup-develop-environment
+    mv setup-develop-environment $1-environment
+    mv $1-environment/Frameworks/$1 $1-environment/
+    cd $1-environment && rm -rf .git
 } # this ensures the entire script is downloaded #
 
