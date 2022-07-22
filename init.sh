99c1d4b4680f0d10671b5439e87a98210330fe72
shabaraba <work.shabaraba@gmail.com>
2022-07-22 14:25:33 +0900

    - fix

--- init.sh
+++ init.sh
@@ -20,6 +20,7 @@
 
     git clone https://github.com/shabaraba/setup-develop-environment \
         && mv setup-develop-environment $1-environment \
+        && cd $1-environment \
         && mv Frameworks/$1/* ./ \
         && rm -rf Frameworks .git
 }
