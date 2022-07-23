6373a6218abd98d4056bf3637295b14266d5bb42
shabaraba <work.shabaraba@gmail.com>
2022-07-23 15:07:29 +0900

    - add all environment framework

--- init.sh
+++ init.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
-{ # this ensures the entire script is downloaded #
-    # setup_laravel() {
-    #     git clone https://github.com/shabaraba/setup-develop-environment
-    #     mv setup-develop-environment laravel-environment
-    #     mv laravel-environment/Frameworks/Laravel laravel-environment/
-    #     cd laravel-environment && rm -rf .git
-    # }
-    #
-    # setup_react_native() {
-    #     git clone https://github.com/shabaraba/react-native-environment.git
-    #     cd react-native-environment && rm -rf .git
-    # }
-    #
-    # if [ $# -ne 1 ]; then
-    #   echo "the number of this shell argument must be 1." 1>&2
-    #   exit 1
-    # fi
-    #
+{
+    echo 'git cloning...'
+    git clone https://github.com/shabaraba/setup-develop-environment
+    mv ./setup-develop-environment $1-environment
 
-    git clone https://github.com/shabaraba/setup-develop-environment \
-        && mv ./setup-develop-environment/Frameworks/$1/* ./
+    echo 'select target framework environments... ./setup-develop-environment/Frameworks/'$1
+    mv $1-environment/Frameworks/$1/* $1-environment/
+    rm -rf $1-environment/Frameworks $1-environment/.git $1-environment/init.sh $1-environment/LICENSE $1-environment/README.md
+
+    touch $1-environment/README.md
+    mkdir $1-environment/src && touch $1-environment/src/empty
+    mkdir $1-environment/database && touch $1-environment/database/empty
 }
+
+
 
